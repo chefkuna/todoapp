6b7adb5597497abaf817011e7e863eb0069bf275
chefkuna <sukhyun94@naver.com>
2022-10-25 00:23:26 +0900

    Darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
@@ -10,8 +11,10 @@ function App() {
 
   return (
     <>
-      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
-      <TodoList filter={filter} />
+      <DarkModeProvider>
+        <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+        <TodoList filter={filter} />
+      </DarkModeProvider>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {!darkMode && <FaMoon />}
+        {darkMode && <FaSun />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
