d9ea7b0d1556d7f6f665f04c099fb409d35498a9
chefkuna <sukhyun94@naver.com>
2022-10-24 21:42:54 +0900

    ADD, DELETE, FILTER

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css';
-import React from 'react';
-import TodoList from './components/TodoList/TodoList';
+import "./App.css";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList/TodoList";
+import Header from "./components/Header/Header";
+
+const filters = ["all", "active", "completed"];
 
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
+
   return (
     <>
-    <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </>
   );
 }
